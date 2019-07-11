86a943bdc195378930aae930b99f26abb3fb83eb
Markus Madeja <kauto@gmx.de>
2019-07-11 10:03:02 +0200

    give deltatime to callback sprite

--- src/lib/Sprites/Callback.mjs
+++ src/lib/Sprites/Callback.mjs
@@ -1,15 +1,22 @@
-import Circle from './Circle.mjs';
+import Circle from "./Circle.mjs";
 
-export default class Callback extends Circle{
-    constructor(params) {
-        super(params);
-        // Callback
-		this.callback = params.callback;
-    }
+export default class Callback extends Circle {
+  constructor(params) {
+    super(params);
+    // Callback
+    this.callback = params.callback;
+    this.timepassed = 0;
+  }
 
-    draw(context, additionalParameter) {
-		if (this.enabled) {
-			this.callback(context, additionalParameter, this);
-		}
+  animate(timepassed) {
+    this.timepassed += timepassed;
+    super.animate(timepassed);
+  }
+
+  draw(context, additionalParameter) {
+    if (this.enabled) {
+      this.callback(context, this.timepassed, additionalParameter, this);
     }
+    this.timepassed = 0;
+  }
 }
